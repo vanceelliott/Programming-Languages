2e5977fb2e1f425a5b9a27194c3cfa96bf0cc4d9
vanceelliott <70769088+vanceelliott@users.noreply.github.com>
2021-01-14 13:59:20 -0500

    Solved problem 1 in myFirstPractice

---
+++ Section 1/myFirstPractice.scm
@@ -0,0 +1,15 @@
+(define (main)
+    (println "Author: Vance Elliott vanceelliott@westminster.net")
+    )
+
+; Problem 1
+(println "the phone number I'm using is 1 (404) 555 2346")
+
+(inspect (/ 550 5 2 55))
+(inspect (+ 202 200 2))
+(inspect (- 1000 445))
+(inspect (* 46 (+ 37 14)))
+
+
+
+
